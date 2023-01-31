27ca1ceee0a7edfffe722bf3a9c5d07b50e1db01
k00laid2 <109769939+k00laid2@users.noreply.github.com>
2023-01-31 17:22:58 -0600

    exercise 1

---
+++ src/exercises/NewClassTesting.java
@@ -0,0 +1,14 @@
+package exercises;
+
+
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class NewClassTesting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sup bud, what's youre name bruh?");
+        String name = input.nextLine();
+        System.out.println("Wasssuuuup " + name);
+    }
+}
