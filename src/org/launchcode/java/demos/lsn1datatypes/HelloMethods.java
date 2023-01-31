c3b72a322dacd58d913e69531e5c13f6c60f8399
k00laid2 <109769939+k00laid2@users.noreply.github.com>
2023-01-31 17:18:27 -0600

    hello methods changes

--- src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
+++ src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
@@ -5,6 +5,10 @@ public class HelloMethods {
     public static void main(String[] args) {
         String message = Message.getMessage("fr");
         System.out.println(message);
+        String message2 = Message.getMessage("sp");
+        System.out.println(message2);
+        String message3 = Message.getMessage("yernan");
+        System.out.println(message3);
     }
 
 }
--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("yernan")) {
+            return "yer nan bruv";
         } else {
             return "Hello, World!";
         }
