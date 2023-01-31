8052051177dfa953812bbcd02bb963bfa56c5cc5
k00laid2 <109769939+k00laid2@users.noreply.github.com>
2023-01-31 17:31:39 -0600

    exercise 3 miles per gallon

---
+++ src/exercises/GasMileage.java
@@ -0,0 +1,15 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class GasMileage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles have you driven?");
+        Integer miles = Integer.valueOf(input.nextLine());
+        System.out.println("How many gallons of gas did that take?");
+        Integer gallons = Integer.valueOf(input.nextLine());
+        double mileage = miles / gallons;
+        System.out.println("The miles per gallon is  " + mileage);
+    }
+}
